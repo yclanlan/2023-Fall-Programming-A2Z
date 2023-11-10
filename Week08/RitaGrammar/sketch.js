a67b98b3d2066cbf4050bf65057fadbcfe325128
yclanlan <lanlan.yichun@gmail.com>
2023-11-10 11:10:48 -0500

    move the defualt text a bit lower

--- Week08/RitaGrammar/sketch.js
+++ Week08/RitaGrammar/sketch.js
@@ -15,7 +15,7 @@ function draw() {
   textSize(4/100*windowWidth);
   text(lines[0], width / 2, 5/10*windowHeight/2);
   textSize(2/100*windowWidth);
-  text(lines[1], width / 2, 6/10*windowHeight/2);
+  text(lines[1], width / 2, 6.5/10*windowHeight/2);
   textSize(2/100*windowWidth);
   text(lines[2], width / 2, 8/10*windowHeight/2);
 
